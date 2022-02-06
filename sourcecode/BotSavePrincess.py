722c4b54e39b57744419eb87f5aecfec27763d40
mkaanmolla <93287714+mkaanmolla@users.noreply.github.com>
2022-02-06 19:22:19 +0300

    Update BotSavePrincess.py

--- sourcecode/BotSavePrincess.py
+++ sourcecode/BotSavePrincess.py
@@ -2,14 +2,17 @@
 from tracemalloc import start
 import queue
 
-  
+
 
 m = int(input())
 grid = [] 
 for i in range(0, m): 
     grid.append(input().strip())
 
-
+#"validation" is a function for testing the move is valid or not.
+#For example if your grid is like   -m-  bot could go upward however, it is invalid. 
+#                                   ---
+#                                   -p- 
 def validation (grid, moves):
     for j in range(m):
         for x, pos in enumerate(grid[j]):
@@ -34,6 +37,8 @@ def validation (grid, moves):
             return False
     return True
 
+#"findPrincess" is a function for finding the princess.
+# If our bot (m) go to the same position in our grid list it will return true and also prints the moves exactly.
 def findPrincess (grid, moves):
     for j in range(m):
         for x, pos in enumerate(grid[j]):
@@ -66,7 +71,10 @@ def findPrincess (grid, moves):
     return False
 
 
-
+#The following lines show the main algorithm. Queue is a default library in Python 3.
+# put() --> puts an element to the queue
+# get() --> gets an element from the queue
+# Basic BFS algorithm is used in the following lines.
 nums = queue.Queue()
 nums.put("")
 add = ""
